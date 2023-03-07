8ffd55457e5112a5e2f50bc34b254f7cb4fc1164
HuangHEE <hhuining888@gmail.com>
2023-03-07 17:48:57 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# programing_for_ds
+ 2023S1
---
+++ week1/Hello_World.py
@@ -0,0 +1,15 @@
+
+print('Hello World')
+
+#2. Edit your script file to prompt the user for their name (and any other information you might be interested in) and then output to the scree a greeting that includes their name and this other information.
+
+# prompt the user for their name
+name = input('What is your name? ')
+# prompt the user for their age
+age = input('What is your age? ')
+# prompt the user for their favorite color
+color = input('What is your favorite color? ')
+# prompt the user for their favorite animal
+animal = input('What is your favorite animal? ')
+# print a greeting that includes their name and this other information
+print('Hello', name, 'you are', age, 'years old. Your favorite color is', color, 'and your favorite animal is', animal)
